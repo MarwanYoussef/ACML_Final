61815182116c0986d24e58c4d9552f537795cd2b
heemo70 <36470617+heemo70@users.noreply.github.com>
2018-12-03 22:48:15 +0200

    Minor footer updates

--- Client/src/app/@theme/components/footer/footer.component.ts
+++ Client/src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Eat fresh live healthy.</span>
+    <span class="created-by">Eat fresh, live healthy!</span>
   `
 })
 export class FooterComponent {}
